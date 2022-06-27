bd9bffbc646c102e09c4321e1f5ecf5847cd0bee
AfonSir <afonsir17@gmail.com>
2022-06-27 19:23:53 -0300

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -76,4 +76,14 @@ describe('DbLoadAccountByToken Usecase', () => {
 
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
+
+  test('should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBeNull()
+  })
 })
